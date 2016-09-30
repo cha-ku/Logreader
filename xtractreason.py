348a0c4941fd04d626cf69972829b3a8857002d9
chaiku <ckukde@gmail.com>
2016-10-01 00:24:42 +0530

    removed unwanted files

--- xtractreason.py
+++
@@ -1,5 +0,0 @@
-with open('Pamlog.txt') as logfile:
-    for line in logfile.readlines():
-            if "GCSWebLogicEJBProxy" in line:
-                    print "response - ", line.split("|")[9].split(":")[4]
-                    print "detail message - ", line.split("|")[9].split((":")[6]
